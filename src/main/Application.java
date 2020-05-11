af30f394424ee08521fb2a5281b770e6368d1428
Radu Ţurcanu <63318406+Rainbovv@users.noreply.github.com>
2020-05-11 17:15:32 +0300

    Update Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -7,15 +7,9 @@ public class Application {
     public static void main(String[] args){
 
         AbstractRobot vasilii = RobotFactory.getRobot("Charlie","Vasilii");
-        vasilii.setY(100);
-        vasilii.setX(50);
-        vasilii.setCharge(50);
-        vasilii.moveDown();
-        System.out.println(vasilii.getY());
-//        System.out.printf( "Charge test passed %d%%\n", ChargeTest.testCharge(vasilii));
-//        System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase(vasilii));
-//        System.out.printf( "Diagonal walk test passed %d%%\n", DiagonalWalkTest.testDiagonalWalking(vasilii));
 
-
+        System.out.printf( "Charge test passed %d%%\n", ChargeTest.testCharge(vasilii));
+        System.out.printf( "Simple test passed %d%%\n", SimpleTest.runCase(vasilii));
+        System.out.printf( "Diagonal walk test passed %d%%\n", DiagonalWalkTest.testDiagonalWalking(vasilii));
     }
 }
